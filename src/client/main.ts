175545efb2a2860edda9b1534a5b3cd3b5c03130
stringang <14845458+stringang@users.noreply.github.com>
2019-11-14 11:03:31 +0800

    add vuex state management

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
+import store from './store'
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
@@ -8,5 +9,6 @@ Vue.config.devtools = true;
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
---
+++ src/client/store/index.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+// https://github.com/vuejs/vuex
+export default new Vuex.Store({
+  modules: {},
+  state: {},
+  actions: {},
+  mutations: {}
+})
