15ccc69012f0885291594746bd3e71532e47594b
stringang <14845458+stringang@users.noreply.github.com>
2020-03-05 17:50:30 +0800

    fix devServer.ts

--- build/devServer.ts
+++ build/devServer.ts
@@ -1,34 +1,30 @@
-import Koa from 'koa';
-import serve from 'koa-static';
-import path from 'path';
 import webpack from 'webpack';
 import webpackConfig from '../webpack.config';
+import {NestFactory} from "@nestjs/core";
+import {NestExpressApplication} from "@nestjs/platform-express";
+import {AppModule} from "../src/server/app.module";
 
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import koaWebpackDevMiddleware from 'koa-webpack-dev-middleware';
-import koaWebpackHotMiddleware from 'koa-webpack-hot-middleware';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const app = new Koa();
 const compiler = webpack(webpackConfig);
 
+async function bootstrap () {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
 // https://github.com/webpack/webpack-dev-middleware
-app.use(koaWebpackDevMiddleware(compiler, {
-  logLevel: 'warn', publicPath: webpackConfig.output.publicPath
-}));
+  app.use(webpackDevMiddleware(compiler, {
+    logLevel: 'warn', publicPath: webpackConfig.output.publicPath
+  }));
 
-// https://github.com/webpack-contrib/webpack-hot-middleware#installation--usage
-app.use(koaWebpackHotMiddleware(compiler, {
-  log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-}));
+  // https://github.com/webpack-contrib/webpack-hot-middleware#installation--usage
+  app.use(webpackHotMiddleware(compiler, {
+    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+  }));
 
-app.use(serve(resolve('/dist/client')));
-
-app.listen(3000);
-
-
-function resolve(dir) {
-  return path.join(__dirname, '..', dir);
+  await app.listen('3000');
 }
+
+bootstrap();
